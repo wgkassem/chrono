4550cfa78e9540977efde0bf2ff42f1f9ea033b6
wgkassem <wassim.kassem@deepx.co.jp>
2021-12-27 22:44:11 +0200

    add cylindrical coordinate option

--- src/chrono_gpu/physics/ChSystemGpu.cpp
+++ src/chrono_gpu/physics/ChSystemGpu.cpp
@@ -693,6 +693,12 @@ void ChSystemGpuMesh::GetMeshPosition(int mesh, ChVector<>& position, const int&
         N += 3;
     }
     position /= N;
+    if (coord_sys == 1){
+        double nrm = sqrt( position.x() * position.x() + position.y() * position.y() )
+        if (nrm  == 0) {position.Set(0.f, 0.f, position.z()); return;}
+        double theta = acos( position.x() / nrm );
+        position.Set( nrm, theta, position.z());
+    }
 }
 
 void ChSystemGpu::SetBDCenter(const ChVector<float>& O) {
