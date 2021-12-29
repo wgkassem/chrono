2cd6e9ede033add37a03e8a9a4952f2aa7f83a5f
wgkassem <wassim.kassem@deepx.co.jp>
2021-12-29 15:52:40 +0200

    fix error meshposition move function

--- examples/triaxial/src/triaxial_compression.cpp
+++ examples/triaxial/src/triaxial_compression.cpp
@@ -319,7 +319,7 @@ int main(int argc, char* argv[]) {
         sideMeshesPositions.push_back(meshpos);
     }
     
-    double sidePlate_radial_vel = 0.2f;  // cm.s-1
+    double sidePlate_radial_vel = 1.f;  // cm.s-1
     float sidePlate_moveTime = curr_time;
     ChVector<> v0(0.f, 0.f, 0.f);  // place-holder
     ChVector<> w0(0.f, 0.f, 0.f);  // place-holder
@@ -344,7 +344,7 @@ int main(int argc, char* argv[]) {
         
         // Move side plates
         for (unsigned int i=1; i<nmeshes-1; i++){
-            // sidePlate_advancePos(i-1);
+            sidePlate_advancePos(i-1);
             gpu_sys.ApplyMeshMotion(i,sideMeshesPositions[i-1],q0, v0, w0);
         }
         
