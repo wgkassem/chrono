89fc421c3d8e32c7e3c90bcca32a8bcf04ac9be9
wgkassem <wassim.kassem@deepx.co.jp>
2021-12-29 15:45:28 +0200

    fix error meshposition id

--- examples/triaxial/src/triaxial_compression.cpp
+++ examples/triaxial/src/triaxial_compression.cpp
@@ -344,7 +344,7 @@ int main(int argc, char* argv[]) {
         
         // Move side plates
         for (unsigned int i=1; i<nmeshes-1; i++){
-            sidePlate_advancePos(i-1);
+            // sidePlate_advancePos(i-1);
             gpu_sys.ApplyMeshMotion(i,sideMeshesPositions[i-1],q0, v0, w0);
         }
         
