0de2a86d2e6c689355fb2164364a032271b7bef8
wgkassem <wassim.kassem@deepx.co.jp>
2022-01-13 13:02:03 +0200

    tiled cylinder

--- examples/triaxial/src/triaxial_compression.cpp
+++ examples/triaxial/src/triaxial_compression.cpp
@@ -181,10 +181,10 @@ int main(int argc, char* argv[]) {
     //mesh_masses.push_back(mixer_mass);
 
     // add bottom
-    // mesh_filenames.push_back("./models/unit_circle_+z.obj"); // add bottom slice
-    // mesh_rotscales.push_back(mesh_scale); // push scaling - no rotation
-    // mesh_translations.push_back(make_float3(cyl_center.x(), cyl_center.y(), -0.5f * scaling.z)); // push translation
-    // mesh_masses.push_back(mixer_mass); // push mass
+    mesh_filenames.push_back("./models/unit_circle_+z.obj"); // add bottom slice
+    mesh_rotscales.push_back(mesh_scale); // push scaling - no rotation
+    mesh_translations.push_back(make_float3(cyl_center.x(), cyl_center.y(), -0.5f * scaling.z)); // push translation
+    mesh_masses.push_back(mixer_mass); // push mass
 
     // add sides
     // for (int i=0; i<120; ++i){
@@ -194,7 +194,7 @@ int main(int argc, char* argv[]) {
     //     mesh_translations.push_back(make_float3(cyl_center.x(), cyl_center.y(), cyl_center.z())); // no translation for side slab
     //     mesh_masses.push_back(mixer_mass); // push standard mass
     // }
-    unsigned int nrots = 120;
+    unsigned int nrots = 360;
     unsigned int nstacks = 1;
     float dtheta = 360. / nrots; //degrees
     float dz = cell_hgt / nstacks;
@@ -203,7 +203,7 @@ int main(int argc, char* argv[]) {
     unsigned int ntiles = nrots * nstacks;
     ChMatrix33<float> tile_scale(ChVector<float>(base_tile,1.,height_tile));
 
-    for (unsigned int i = 0; i < 2; i++){
+    for (unsigned int i = 0; i < nrots; i++){
         ChQuaternion<> quatRot = Q_from_AngAxis( (float) (i/nstacks) * dtheta * CH_C_DEG_TO_RAD, VECT_Z); // stacked ntriangles
         mesh_filenames.push_back("./models/unit_tritile_-y.obj");
         mesh_rotscales.push_back(ChMatrix33<float>(quatRot) * tile_scale);
@@ -214,7 +214,7 @@ int main(int argc, char* argv[]) {
     // add top
     mesh_filenames.push_back("./models/unit_circle_-z.obj"); // add bottom slice
     mesh_rotscales.push_back(mesh_scale); // push scaling - no rotation
-    mesh_translations.push_back(make_float3(cyl_center.x(), cyl_center.y(), params.box_Z/2.f-1.f)); // push translation top top of box
+    mesh_translations.push_back(make_float3(cyl_center.x(), cyl_center.y(), cell_hgt/2.f+1.f)); // push translation top top of box
     mesh_masses.push_back(mixer_mass); // push mass
 
     std::cout << mesh_filenames.size() << ", "<< mesh_rotscales.size() << ", "<< mesh_translations.size() << ", "<< mesh_masses.size() << "\n";
