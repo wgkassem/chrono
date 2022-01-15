c58a1dad8ff22519bfe3f915f8ea10f9afacdd2c
wgkassem <wassim.kassem@deepx.co.jp>
2022-01-15 10:51:49 +0200

    make base shorter

--- examples/triaxial/src/triaxial_compression.cpp
+++ examples/triaxial/src/triaxial_compression.cpp
@@ -202,7 +202,7 @@ int main(int argc, char* argv[]) {
     unsigned int nstacks = 1;
     float dtheta = 360. / nrots; //degrees
     float dz = cell_hgt / nstacks;
-    float tile_base = M_PI * cell_diam / (float) nrots;
+    float tile_base = M_PI * (cell_diam * 0.75) / (float) nrots;
     float tile_height = cell_hgt / (float) nstacks; // should be a multiple of cell_hgt
     unsigned int ntiles = nrots * nstacks;
     ChMatrix33<float> tile_scale(ChVector<float>(1., tile_base, tile_height));
